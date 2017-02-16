817756ba2b3922e9da272e9150f042b93436fdc0
ghilston <Greg.Hilston@madwiremedia.com>
2017-02-16 13:42:06 -0700

    Working on serving static html files, like index.html

--- handlers.go
+++ handlers.go
@@ -2,10 +2,20 @@ package main
 
 import (
     "fmt"
+    //"io/ioutil"
+    //"time"
+    //"bytes"
     "net/http"
 )
 
 func Index(w http.ResponseWriter, r *http.Request) {
+    //r.ParseForm()
+    //StoredAs := r.Form.Get("index.html") // file name
+    // data, err := ioutil.ReadFile("static/" + StoredAs)
+    // if err != nil { fmt.Fprint(w, err) }
+    // http.ServeContent(w, r, StoredAs, time.Now(),   bytes.NewReader(data))
+
+    // http.ServeFile(w, r, r.URL.Path[1:])
     fmt.Fprintln(w, "Welcome!")
 }
 
